e7029e9d370d0eb701d44c5b9da6c0df4b4a044b
janamoser <s2110456033@fhooe.at>
2024-06-24 15:18:07 +0200

    navigation

--- GeoGuide1.0/app/(geoguide)/_layout.tsx
+++ GeoGuide1.0/app/(geoguide)/_layout.tsx
@@ -2,8 +2,14 @@ import {Stack} from 'expo-router'
 export default function GeoGuideLayout(){
     return(
         <Stack>
-            <Stack.Screen name = "index"/>
-        <Stack.Screen name = "[id]"/>
+            <Stack.Screen name = "index" options={{
+            headerTitle:"GeoGuide",
+            title:"GeoGuide",}
+            }/>
+        <Stack.Screen name = "[id]" options={{
+        headerTitle:"Details",
+        title:"Details",}
+        }/>
             </Stack>
     )
 }
--- GeoGuide1.0/app/_layout.tsx
+++ GeoGuide1.0/app/_layout.tsx
@@ -1,10 +1,42 @@
-import {Tabs} from "expo-router";
+import {SplashScreen, Tabs} from "expo-router";
+import {useFonts} from "expo-font";
+import {useEffect} from "react";
 
-export default function TabsLayout(){
-    return(
+SplashScreen.preventAutoHideAsync();
+export default function RootLayout() {
+    const [loaded, error] = useFonts({
+        'nunito-regular':require('../assets/fonts/Nunito-Regular.ttf'),
+        'nunito-bold':require('../assets/fonts/Nunito-Bold.ttf'),
+    });
+
+    // Expo Router uses Error Boundaries to catch errors in the navigation tree.
+    useEffect(() => {
+        if (error) throw error;
+    }, [error]);
+
+    useEffect(() => {
+        if (loaded) {
+            SplashScreen.hideAsync();
+        }
+    }, [loaded]);
+
+    if (!loaded) {
+        return null;
+    }
+
+    return <TabsLayout />;
+}
+function TabsLayout() {
+    return (
         <Tabs>
-            <Tabs.Screen name = ""/>
-            <Tabs.Screen name = "profile"/>
+            <Tabs.Screen name="(geoguide)" options={{
+                headerShown: false,
+                title: "GeoGuide"
+            }}/>
+            <Tabs.Screen name="profile" options={{
+                headerTitle: "Profile",
+                title: "Profile"
+            }}/>
         </Tabs>
     )
 }
